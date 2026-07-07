19cb17d719ca0b4e1d50545b89808db1169b55ef
advancecuser2005 <mailtodaksh@vt.edu>
2026-07-07 18:34:46 -0400

    Practice Cmake and Testing

---
+++ practice/build/CMakeFiles/Practice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practice.dir/link.d"
+  "CMakeFiles/Practice.dir/src/main.cpp.o"
+  "CMakeFiles/Practice.dir/src/main.cpp.o.d"
+  "Practice"
+  "Practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
