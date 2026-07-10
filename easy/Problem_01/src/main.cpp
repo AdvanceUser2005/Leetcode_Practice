8ca97a39a0c0bf83e9df44a7ea923d9d1b3b9df9
advancecuser2005 <mailtodaksh@vt.edu>
2026-07-10 14:24:13 -0400

    Fixed a few things

--- easy/Problem_01/src/main.cpp
+++ easy/Problem_01/src/main.cpp
@@ -4,12 +4,24 @@
 int main() {
     
     // Defining a  test case to practice with
-    int array[5] = {2, 7, 11, 15};
+    int array[] = {2, 7, 11, 15};
     int target = 9; // Defined target for tthe test case
 
     int size = sizeof(array) / sizeof(array[0]);
+    
+    // to find the sum pairs, it has to for every number in the array and find the number. 
+    // 9 - 2 = 7
+    //pairs are 2 and 7
 
-    std::cout << size << std::endl;
+    for (int i = 0; i < size; i++) {
+        int num1 = target - array[i];
+        if (array[i] = num1) {
+            int num1pair = array[i];
+        }
+        std::cout << array[i] << " " << num1 << std::endl;
+    }
+
+    std::cout << "testing" << std::endl;
 
     return 0;
 
