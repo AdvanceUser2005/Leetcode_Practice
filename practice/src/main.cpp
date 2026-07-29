09cf2dd0bb17d5247f1d3d60dd5cea76051a2d4e
advancecuser2005 <mailtodaksh@vt.edu>
2026-07-29 10:28:10 -0400

    Square patter has been printed

--- practice/src/main.cpp
+++ practice/src/main.cpp
@@ -104,5 +104,18 @@ int main() {
     int n;
     cin >> n;
 
-    
+    for (int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
+            cout << i;
+            if (i == n) {
+                break;
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+        
+    }
+
+    return 0;
 }
