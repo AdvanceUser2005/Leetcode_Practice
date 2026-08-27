8050b0e28eb906fca8f5fc15cbb07f010321c46c
advancecuser2005 <mailtodaksh@vt.edu>
2026-08-27 12:30:37 -0400

    Two Sums Problem 1 completed

--- easy/Problem_01/src/main.cpp
+++ easy/Problem_01/src/main.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> sol;
         for(int j = 0; j < nums.size(); j++) {
             for (int i = 0; i < nums.size(); i++) {
-                if (nums.at(j) + nums.at(i) == target) {
+                if (nums.at(j) + nums.at(i) == target && i != j) {
                     sol.push_back(j);
                     sol.push_back(i);
                     return sol;
@@ -15,3 +20,9 @@ public:
         return sol;
     };
 };
+
+int main() {
+    //Test cases
+
+
+}
