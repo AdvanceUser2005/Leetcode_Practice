f1554cfe8e70e690cefd950a99401197c23a0c9f
advancecuser2005 <mailtodaksh@vt.edu>
2026-08-27 12:47:41 -0400

    Efficient solution for two sums problem

--- easy/Problem_01/src/main.cpp
+++ easy/Problem_01/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -26,3 +27,18 @@ int main() {
 
 
 }
+
+class SolutionEfficient {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) {
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
